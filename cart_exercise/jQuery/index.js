ac0aa2ebde3d43d737c4814f8fa10789cfdc2ec5
vaishalittn <vaishali@tothenew.com>
2018-11-17 21:28:00 +0530

    my assignment

---
+++ cart_exercise/jQuery/index.js
@@ -0,0 +1,65 @@
+
+$(document).ready(function(){
+    $(".special-buttons > #shoe").on('click',function(){
+        $(".special-category > .specials").hide();
+        $(".special-buttons > .specials").css({
+            'color':'#979797',
+            'border-top':'6px solid #000000'
+        });
+        $(".special-category > #shoe-list").css("display" , "block");
+        $(".special-buttons > #shoe").css("border-top","6px solid #57c5a0");
+        $(".special-buttons > #shoe").css("color" , "#f5eded");
+    });
+});
+
+$(document).ready(function(){
+    $(".special-buttons > #anna").on('click',function(){
+        $(".special-category > .specials").hide();
+        $(".special-buttons > .specials").css({
+            'color':'#979797',
+            'border-top':'6px solid #000000'
+        });
+        $(".special-category > #anna-list").css("display" , "block");
+        $(".special-buttons > #anna").css("border-top","6px solid #57c5a0");
+        $(".special-buttons > #anna").css("color" , "#f5eded");
+    });
+});
+
+$(document).ready(function(){
+    $(".special-buttons > #prada").on('click',function(){
+        $(".special-category > .specials").hide();
+        $(".special-buttons > .specials").css({
+            'color':'#979797',
+            'border-top':'6px solid #000000'
+        });
+        $(".special-category > #prada-list").css("display" , "block");
+        $(".special-buttons > #prada").css("border-top","6px solid #57c5a0");
+        $(".special-buttons > #prada").css("color" , "#f5eded");
+    });
+});
+
+$(document).ready(function(){
+    $(".special-buttons > #casadei").on('click',function(){
+        $(".special-category > .specials").hide();
+        $(".special-buttons > .specials").css({
+            'color':'#979797',
+            'border-top':'6px solid #000000'
+        });
+        $(".special-category > #casadei-list").css("display" , "block");
+        $(".special-buttons > #casadei").css("border-top","6px solid #57c5a0");
+        $(".special-buttons > #casadei").css("color" , "#f5eded");
+    });
+});
+
+$(document).ready(function(){
+    $(".special-buttons > #mellow").on('click',function(){
+        $(".special-category > .specials").hide();
+        $(".special-buttons > .specials").css({
+            'color':'#979797',
+            'border-top':'6px solid #000000'
+        });
+        $(".special-category > #mellow-list").css("display" , "block");
+        $(".special-buttons > #mellow").css("border-top","6px solid #57c5a0");
+        $(".special-buttons > #mellow").css("color" , "#f5eded");
+    });
+});
